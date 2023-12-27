915a8c78a49edae7f38b79a3544da55b3a7b40a2
tomguillermou <tomguillermou@gmail.com>
2023-12-27 18:33:15 +0100

    Add NgRx dev tools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState, provideStore } from '@ngrx/store';
@@ -6,7 +6,14 @@ import { provideState, provideStore } from '@ngrx/store';
 import { appReducer, appEffects } from '@libs/store';
 
 import { routes } from './app.routes';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideState('app', appReducer), provideEffects(appEffects)],
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideState('app', appReducer),
+    provideEffects(appEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
